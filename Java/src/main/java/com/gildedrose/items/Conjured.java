fb3e2066a8695638444e417a78725c845651eeec
Nele Scherrens <nele.scherrens@unifiedpost.com>
2021-04-09 16:11:38 +0200

    Add the Conjured item

---
+++ Java/src/main/java/com/gildedrose/items/Conjured.java
@@ -0,0 +1,27 @@
+package com.gildedrose.items;
+
+import com.gildedrose.Item;
+import com.gildedrose.helpers.GildedRoseConstants;
+
+/**
+ * User: nelescherrens {@literal <nele.scherrens@unifiedpost.com>}
+ */
+public class Conjured {
+
+    public static Item update(Item itemToUpdate) {
+        int quality = itemToUpdate.quality;
+        int sellIn = itemToUpdate.sellIn;
+
+        if (quality < GildedRoseConstants.MAX_QUALITY) {
+            quality = quality - 2;
+        }
+
+        sellIn = sellIn - 1;
+
+        if (sellIn < 0 && quality < GildedRoseConstants.MAX_QUALITY) {
+            quality = quality - 2;
+        }
+
+        return new Item(GildedRoseConstants.CONJURED, sellIn, quality);
+    }
+}
