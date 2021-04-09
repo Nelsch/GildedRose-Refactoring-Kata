0024690efe4f9809aa5ea83c528d648ba5102179
Nele Scherrens <nele.scherrens@unifiedpost.com>
2021-04-09 16:11:50 +0200

    Add the name for the Conjured item

--- Java/src/main/java/com/gildedrose/helpers/GildedRoseConstants.java
+++ Java/src/main/java/com/gildedrose/helpers/GildedRoseConstants.java
@@ -8,6 +8,7 @@ public class GildedRoseConstants {
     public static final String AGED_BRIE = "Aged Brie";
     public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String CONJURED = "Conjured";
 
     public static final int MAX_QUALITY = 50;
 }
