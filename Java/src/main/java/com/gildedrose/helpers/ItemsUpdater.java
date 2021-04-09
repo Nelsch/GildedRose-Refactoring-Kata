c0b5f8d2568e69b6aa3acd96190bafd93fa19d60
Nele Scherrens <nele.scherrens@unifiedpost.com>
2021-04-09 15:03:28 +0200

    Introduce ItemsUpdater class

---
+++ Java/src/main/java/com/gildedrose/helpers/ItemsUpdater.java
@@ -0,0 +1,29 @@
+package com.gildedrose.helpers;
+
+import com.gildedrose.Item;
+import com.gildedrose.items.AgedBrie;
+import com.gildedrose.items.BackstagePasses;
+import com.gildedrose.items.Others;
+
+/**
+ * User: nelescherrens {@literal <nele.scherrens@unifiedpost.com>}
+ */
+public class ItemsUpdater {
+
+    public static Item updateItem(Item item) {
+        Item updatedItem;
+        String itemName = item.name;
+
+        if (GildedRoseConstants.AGED_BRIE.equals(itemName)) {
+            updatedItem = AgedBrie.update(item);
+        } else if (GildedRoseConstants.BACKSTAGE_PASSES.equals(itemName)) {
+            updatedItem = BackstagePasses.update(item);
+        } else if (GildedRoseConstants.SULFURAS.equals(itemName)) {
+            updatedItem = item;
+        } else {
+            updatedItem = Others.update(item);
+        }
+
+        return updatedItem;
+    }
+}
